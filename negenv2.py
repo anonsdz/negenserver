b5a1eddfff7878a878d6124d4d4cc98032004399
anonsdz <61068030+anonsdz@users.noreply.github.com>
2024-12-13 14:33:14 +0700

    Update negenv2.py

--- negenv2.py
+++ negenv2.py
@@ -6,7 +6,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 from pytz import timezone
 from html import escape
 
-TOKEN = '7918986015:AAEFeUIH4diM4noN_00CGlFtgPSWTjRVTGc'
+TOKEN = '7534473375:AAEu7g1K31B4ckivIqKEz0Ecx49XPOGLrLs'
 ADMIN_ID = 7371969470
 VIP_USERS_FILE, METHODS_FILE, GROUPS_FILE = 'vip_users.json', 'methods.json', 'groups.json'
 user_processes = {}
